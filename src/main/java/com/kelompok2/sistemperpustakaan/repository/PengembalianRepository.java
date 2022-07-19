aa7508495cd773e56a15308b20a4ae2182b39389
Amsal <amsalwah@gmail.com>
2022-07-19 15:03:27 +0700

    Menambahkan fitur save Pengembalian

--- src/main/java/com/kelompok2/sistemperpustakaan/repository/PengembalianRepository.java
+++ src/main/java/com/kelompok2/sistemperpustakaan/repository/PengembalianRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PengembalianRepository extends JpaRepository<Pengembalian, String > {
+public interface PengembalianRepository extends JpaRepository<Pengembalian, Integer > {
 
-    Optional<Pengembalian> findByIdpengembalian (Integer idpengembalian);
 }
